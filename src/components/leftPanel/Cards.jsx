95bd7ded2425469b47ae9ae10e81cf1d2c6bc547
PranavSharma-1670 <pranavsharma1670@gmail.com>
2025-09-20 11:33:57 +0530

    The DashBoard that was presented

--- src/components/leftPanel/Cards.jsx
+++ src/components/leftPanel/Cards.jsx
@@ -5,20 +5,21 @@ const Cards = () => {
     <div className="row mb-4">
       <div className="col-md-4">
         <div className="card shadow-sm p-3 h-100">
-          <h5>Card 1</h5>
-          <p>Some details here</p>
+          <h5>No of Students Records: </h5>
+          <h2>375</h2>
         </div>
       </div>
       <div className="col-md-4">
         <div className="card shadow-sm p-3 h-100">
-          <h5>Card 2</h5>
-          <p>Some details here</p>
+          <h5>No of Request left unresolved :</h5>
+          <h2>3</h2>
         </div>
       </div>
       <div className="col-md-4">
         <div className="card shadow-sm p-3 h-100">
-          <h5>Card 3</h5>
-          <p>Some details here</p>
+          <h5>Process Back Logs :- </h5>
+          <p> Online Backlog : 1 </p>
+          <p>  Video Backlog : 0 </p>
         </div>
       </div>
     </div>
