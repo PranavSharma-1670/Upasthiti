71f54ad7e96b49fe6426adeb66c3b97a7a5b2255
PranavSharma-1670 <pranavsharma1670@gmail.com>
2025-09-23 19:51:10 +0530

    Some Mid Changes while Connecting

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // backend/server.js
 import express from "express";
 import cors from "cors";
-import { testConnection } from "./db.js";
+import { pool , testConnection} from "./db.js";
 
 const app = express();
 const PORT = 5050;
@@ -17,6 +17,31 @@ app.get("/api/test", async (req, res) => {
     res.status(500).json({ success: false, message: "PostgreSQL connection failed" });
   }
 });
+
+app.get("/api/events", async (req, res) => {
+    try {
+      const result = await pool.query("SELECT * FROM events_and_notices ORDER BY date ASC");
+      res.json(result.rows);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ error: "Server error" });
+    }
+  });
+
+app.post("/api/events", async (req, res) => {
+    const { title, description, date, type, issued_by } = req.body;
+    try {
+      const result = await pool.query(
+        "INSERT INTO events_and_notices (title, description, date, type, issued_by) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        [title, description, date, type, issued_by]
+      );
+      res.json(result.rows[0]);
+    } catch (err) {
+      console.error("Insert error:", err.message);
+      res.status(500).json({ error: "Server error" });
+    }
+  });
+  
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
